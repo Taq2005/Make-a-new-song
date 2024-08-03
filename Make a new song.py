397f544ebe6a0287348e9746bef2851649c89bbc
Taq2005 <muhammadtaqi747@gmail.com>
2024-08-03 23:07:58 +0500

    Add files via upload

---
+++ Make a new song.py
@@ -0,0 +1,5 @@
+print("Write any lyrics and change words with your name")
+x=input("Enter your name:\n")
+y=input("Enter the lyrics:\n")
+z=input("Enter the word you want to change:\n")
+print(y.replace(z, x)) #inverted comma around a variable make it a string
